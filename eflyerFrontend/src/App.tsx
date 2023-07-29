0b503937730c66c4aac1dcf84201c0e5f83866a0
himanshu4776 <hk91619@gmail.com>
2023-07-29 16:27:03 +0530

    home page style created

--- eflyerFrontend/src/App.tsx
+++ eflyerFrontend/src/App.tsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { AlignJustify, Search, ShoppingCart, User2 } from "lucide-react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className='bg-red-500'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-yellow-300 h-screen">
+      <div className="flex justify-center relative">
+        <div className="border-solid items-center justify-center relative flex border-t-[60px] border-l-8 border-l-transparent border-r-transparent border-r-8 w-4/5 h-0 border-slate-800">
+          <div className="text-white font-mono flex flex-row absolute">
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              Best Sellers
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              Gift Ideas
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              New Releases
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              today's Deals
+            </a>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              Customer Service
+            </a>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="font-sans flex items-center justify-center m-10 text-white font-bold text-3xl">
+        Eflyer
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="flex justify-around">
+        <AlignJustify color="#ffffff" />
+        <select
+          placeholder="All category"
+          aria-label="All category"
+          name="category"
+          id="category"
+          className="bg-slate-800 text-white font-mono"
+        >
+          <option value="option1">option1</option>
+        </select>
+        <div className="flex flex-row">
+        <input type="text" name="search" id="search" placeholder="Search this blog" />
+          <input type="button" name="dkashdj" id="jeb" />
+          <label htmlFor="jeb" className="bg-red-400"><Search color="#ffffff" height={20} width={20} /></label>
+        </div>
+        <select
+          placeholder="language"
+          aria-label="select lnguage"
+          name="language"
+          id="language"
+          className="bg-white text-black font-mono"
+        >
+          <option value="option1">English</option>
+        </select>
+        <div id="cart" className="flex text-white">
+          <ShoppingCart color="#ffffff" />
+          <label htmlFor="cart" className="pl-2">
+            Cart
+          </label>
+        </div>
+        <div id="user" className="flex text-white">
+          <User2 />
+          <label htmlFor="user">User</label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
